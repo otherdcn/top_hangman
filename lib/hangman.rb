6c1fb5c643779a2a9b175361040516ad37482979
otherdcn <danny.c.ntwari@gmail.com>
2024-07-12 19:20:12 +0200

    Add functionality to set guesser every new round

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,6 @@
 require_relative "dictionary_list"
 require_relative "player"
+require "colorize"
 
 module Hangman
   class Game
@@ -13,11 +14,26 @@ module Hangman
                     end
     end
 
-    def play
+    def play(rounds = 2)
       puts "Welcome #{player_one.name} and #{player_two.name}!"
+
+      rounds.times do |round|
+        puts "\n******************** Round #{round + 1} ********************".black.on_white
+        set_guesser(round)
+        puts "===> Round: #{round + 1}"
+        puts "===> Guesser: #{guesser.name}"
+      end
+    end
+
+    def set_guesser(round)
+      if round.even?
+        @guesser = player_one
+      else
+        @guesser = player_two.instance_of?(Human) ? player_two : player_one
+      end
     end
   end
 end
 
-game = Hangman::Game.new
+game = Hangman::Game.new(1)
 game.play
